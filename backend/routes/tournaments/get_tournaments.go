7a111335f7b760d378ee3f1690345119e12d4ffc
Maximilian Kruse <maxkruse1996@gmail.com>
2021-09-24 14:04:40 +0200

    Correctly differentiate between visible and staff

--- backend/routes/tournaments/get_tournaments.go
+++ backend/routes/tournaments/get_tournaments.go
@@ -5,14 +5,22 @@ import (
 	"github.com/maxkruse/magnusopus/backend/globals"
 	"github.com/maxkruse/magnusopus/backend/structs"
 	"github.com/maxkruse/magnusopus/backend/utils"
+	"github.com/sirupsen/logrus"
 )
 
 func GetTournaments(c *fiber.Ctx) error {
-	tournaments := []*structs.Tournament{}
+	visible_tournaments := []*structs.Tournament{}
+	staff_tournaments := []*structs.Tournament{}
 	localDB := globals.DBConn
 	self, _ := utils.GetSelf(c)
 
-	localDB.Joins("JOIN staffs ON staffs.tournament_id = tournaments.id JOIN users ON users.id = staffs.user_id").Preload("Rounds").Where("visible = ?", true).Or("users.ripple_id = ?", self.RippleId).Find(&tournaments)
+	localDB.Where("visible = ?", true).Find(&visible_tournaments)
+	localDB.Joins("LEFT JOIN staffs ON staffs.tournament_id = tournaments.id JOIN users ON users.id = staffs.user_id").Where("users.ripple_id = ?", self.RippleId).Find(&staff_tournaments)
 
-	return c.Status(fiber.StatusOK).JSON(tournaments)
+	globals.Logger.WithFields(logrus.Fields{
+		"visible_tournaments": visible_tournaments,
+		"staff_tournaments":   staff_tournaments,
+	}).Info("GetTournaments")
+
+	return c.Status(fiber.StatusOK).JSON(append(visible_tournaments, staff_tournaments...))
 }
